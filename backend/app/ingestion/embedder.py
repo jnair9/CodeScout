347c1e878cfd1f6d70a689fd5ed621cbace18bec
jnair9 <143296724+jnair9@users.noreply.github.com>
2026-06-04 08:30:35 -0400

    set up ingestion

---
+++ backend/app/ingestion/embedder.py
@@ -0,0 +1,15 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+load_dotenv() 
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+def embedder(content):
+    result = client.models.embed_content(
+            model="gemini-embedding-2",
+            contents=content
+    )
+
+    print(result.embeddings)
+    return result.embeddings[0].values
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from .models.schema import IngestRequest
 from .api.ingest import run_ingestion
+from .ingestion.embedder import embedder
+from .ingestion.vector_store import store
 
 
 app = FastAPI()
@@ -9,8 +11,18 @@ app = FastAPI()
 @app.post("/ingest/")
 async def trigger_ingestion(request: IngestRequest):
     ingested_chunks = run_ingestion(str(request.repo_url))
+    ingested_embeddings = [] 
+    for chunk in ingested_chunks:
+        content = None
+        if chunk.parent_class:
+            content = f"title: {chunk.parent_class}.{chunk.symbol_name} | text: {chunk.content}"
+        else:
+            content = f"title: {chunk.symbol_name} | text: {chunk.content}"
+        embedded_content = embedder(content)
+        ingested_embeddings.append(embedded_content)
+    store(ingested_chunks, ingested_embeddings)
     return {
         "message": "Ingestion started for repo.",
-        "url" : str(request.repo_url),
+        "url" : str(request.repo_url), 
         "num chunks ingested" : len(ingested_chunks)
     }
